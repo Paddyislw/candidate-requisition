7b3a3f890e34d79fabd3d7c875329e8d38ed6710
Paddyislw <pradyum0701@gmail.com>
2023-07-27 15:55:53 +0530

    fixed a bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import ParentForm from "@/components/ParentForm";
 import HomeLayout from "@/containers/home/HomeLayout";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
@@ -7,7 +6,6 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      {/* <ParentForm /> */}
       <ChakraProvider>
         <HomeLayout />
       </ChakraProvider>
